d56ff0a5c3fb00d1743bfe4fc5601c66d9891d76
liuwenqi528 <410587745@qq.com>
2017-11-27 13:28:51 +0800

    test "soft"属性

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,8 +8,8 @@
 
 public class HelloWorld {
     public void test(){
-        System.out.println("测试git上的项目下载到idea中，又上传修改后的文件");
-        System.out.println("刚才测试失败了，重新试试！");
+        int a = 10;
+        String str = "测试soft";
         System.out.println("测试使用idea自带继承命令测试撤销commit");
     }
 }
