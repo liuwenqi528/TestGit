56875b0b5300519ee322af08db23b7d6f37a13b2
liuwenqi528 <410587745@qq.com>
2017-11-27 13:36:49 +0800

    add attribute money

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,6 @@
 public class HelloWorld {
     public void test(){
         int a = 10;
-        String str = "测试soft";
         double money = 200.001;
         System.out.println("测试使用idea自带继承命令测试撤销commit");
     }
